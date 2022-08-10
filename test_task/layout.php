85ae772420885d2d3ee5eb44919cdd5b668c119d
Anastasiya Vasileva <81408976+stvasileva@users.noreply.github.com>
2022-08-10 10:48:12 +0300

    Create layout.php

---
+++ test_task/layout.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+<html>
+    <head>
+        <title>Test Vasileva Anastasiya</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" media="screen" type="text/css" href="styles_1.css">
+    </head>
+    <body>
+       <header>
+               <div id="link_main">
+                <a href="/test_1/index.php">Панель управления</a>
+               </div>
+       </header>       
+        <main>
+   
+        </main>
+        <footer>
+    <p class="copyright">© Vasileva Anastasiya</p>
+</footer>
+
+    </body>
+</html>

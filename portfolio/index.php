c2e962c596a2b2fc9a5b8a7175d66af41ec5f4dc
Anastasiya Vasileva <81408976+stvasileva@users.noreply.github.com>
2022-08-10 11:11:27 +0300

    Create index.php

---
+++ portfolio/index.php
@@ -0,0 +1,16 @@
+<?php
+    //Подключение шапки
+    require_once("content/header.php");
+?>
+ <!-- Плагин particles.js -->
+ <div id="particles" class="particles">     
+<div id="content">
+    
+</div>
+</div> 
+
+<?php
+    //Подключение футера
+    require_once("content/footer.php");
+?>
+
